7fb287798fe7c4f6cd43c89067165d853511bce3
katokdoescode <snakedoc.md@gmail.com>
2022-02-09 11:24:46 +0300

    Created RadioBtn common component

--- src/frontend/src/common/components/RadioButton.vue
+++ src/frontend/src/common/components/RadioButton.vue
@@ -1,7 +1,31 @@
 <template>
-  <div></div>
+  <label>
+    <input
+      type="radio"
+      :name="radioBtnName"
+      :value="radioBtnValue"
+      :class="radioBtnClass"
+    />
+    <slot></slot>
+  </label>
 </template>
 <script>
-export default {};
+export default {
+  name: "RadioButton",
+  props: {
+    radioBtnClass: {
+      type: Array,
+      required: false,
+    },
+    radioBtnName: {
+      type: String,
+      required: true,
+    },
+    radioBtnValue: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+};
 </script>
 <style></style>
