67f8f8da0b255311ce8a2179a4648892a90fc548
katokdoescode <snakedoc.md@gmail.com>
2022-02-09 13:01:20 +0300

    Added price prop

--- src/frontend/src/modules/builder/components/BuilderPriceCounter.vue
+++ src/frontend/src/modules/builder/components/BuilderPriceCounter.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="content__result">
-    <p>Итого: 0 ₽</p>
+    <p>Итого: {{ price }} ₽</p>
     <button type="button" class="button" disabled>Готовьте!</button>
   </div>
 </template>
 <script>
 export default {
   name: "PriceCounter",
+  props: {
+    price: {
+      type: Number,
+      default: 0,
+    },
+  },
 };
 </script>
 <style></style>
