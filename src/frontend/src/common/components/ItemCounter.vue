a141bf8525b87f08373243ab55cf16e3442f536e
katokdoescode <snakedoc.md@gmail.com>
2022-02-17 11:21:29 +0300

    Updated main method for changing count

--- src/frontend/src/common/components/ItemCounter.vue
+++ src/frontend/src/common/components/ItemCounter.vue
@@ -3,22 +3,23 @@
     <button
       type="button"
       class="counter__button counter__button--minus"
-      @click="changeCount(false)"
+      @click="changeCount(count - 1)"
       :disabled="count <= 0"
     >
       <span class="visually-hidden">Меньше</span>
     </button>
     <input
+      ref="counterInput"
       type="text"
       name="counter"
       class="counter__input"
       :value="count"
-      @input="changeCount"
+      @input="changeCount($event.target.value)"
     />
     <button
       type="button"
       class="counter__button counter__button--plus"
-      @click="changeCount(true)"
+      @click="changeCount(count + 1)"
       :disabled="count >= 3"
     >
       <span class="visually-hidden">Больше</span>
@@ -39,19 +40,13 @@ export default {
     },
   },
   methods: {
-    changeCount(e) {
-      if (e === true) {
-        this.$emit("changeCount", e);
-      } else if (e === false) {
-        this.$emit("changeCount", e);
-      } else {
-        this.$emit("changeCount", {
-          id: this.item.id,
-          count: Number(e.target.value),
-          price: Number(this.item.price),
-          image: this.item.image,
-        });
-      }
+    changeCount(value) {
+      this.$emit("changeCount", {
+        id: this.item.id,
+        count: Number(value),
+        price: Number(this.item.price),
+        image: this.item.image,
+      });
     },
   },
 };
